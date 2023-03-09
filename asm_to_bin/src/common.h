18f615f6b10917bb86ec3be5edff33c979465bcb
TerrificTable <omegagaming189@gmail.com>
2023-03-09 17:01:02 +0100

    utility for converting asm to binary file (in the future)

---
+++ asm_to_bin/src/common.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef COMMON_H
+#define COMMON_H
+
+
+#define unused [[maybe_unused]]
+
+
+#endif
+
---
+++ asm_to_bin/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "common.h"
+
+
+int main (unused int argc, unused char *argv[]) {
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
